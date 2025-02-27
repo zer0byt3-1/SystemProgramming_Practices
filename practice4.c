256bd7321ad54355de28584307c22fa4047f99fe
zer0byt3 <serverhacked.rev@gmail.com>
2025-02-27 15:37:35 +0300

    Practice 4, continues

--- practice4.c
+++ practice4.c
@@ -5,38 +5,72 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-void SearchBinary(int num)
-{
-    if (!(num >= 0 && num <= 65535)) {
-        printf("Entered value: %d not in range (0-65535)", num);
-        return;
-    }
+#include <stdlib.h>
+void printBinary(unsigned int num, int bits) {
+	for (int i = bits - 1; i >= 0; i--) {
+		printf("%d", (num >> i) & 1);
+	}
+	printf("\n");
+}
+
+void printBitGrid(unsigned int num, int rows, int cols) {
+    printf("Bit grid representation:\n");
+    int maxBits = rows * cols;
     
-    char symbols[] = "01";
-    int num0 = num;
-    int index = 0;
-    char result[128];
-	while (num0 > 0) {
-		int remainder = num0 % 2;
-		result[index++] = symbols[remainder];
-		num0 /= 2;
+    for (int i = 0; i < rows; i++) {
+        for (int j = cols - 1; j >= 0; j--) {
+            int bitIndex = (i * cols) + j;
+            if (bitIndex < maxBits) {
+                printf("%d ", (num >> (maxBits - 1 - bitIndex)) & 1);
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int canStoreNumber(int num, int bytes, int signedFlag) {
+	int maxSize = signedFlag ? (1 << (bytes * 8 - 1)) - 1 : (1 << (bytes * 8)) - 1;
+	int minSize = signedFlag ? -(1 << (bytes * 8 - 1)) : 0;
+
+	if (signedFlag) {
+		return (num <= maxSize && num >= minSize);
 	}
-	printf("Result:");
-	for (int i = index - 1; i >= 0; i--) {
-		putchar(result[i]);
+	else {
+		return (num >= minSize && num <= maxSize);
 	}
-	
-	int controlbitsposs[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
 }
 
 int main()
 {
-	int inputval = 0;
-	scanf("%i", &inputval);
-	SearchBinary(inputval);
+	int num;
+	int bytes;
+	int signedFlag;
+
+	printf("Enter an integer number: ");
+	scanf("%d", &num);
+
+	printf("Enter the number of bytes to store the number (1, 2, 4): ");
+	scanf("%d", &bytes);
+
+	printf("Enter the number type (1 for signed, 0 for unsigned): ");
+	scanf("%d", &signedFlag);
+	if (canStoreNumber(num, bytes, signedFlag)) {
+		int bits = bytes * 8;
+		if (signedFlag) {
+			printf("Binary code of the number %d (signed): ", num);
+			printBinary((unsigned int)(num & ((1 << bits) - 1)), bits);
+		}
+		else {
+			printf("Binary code of number %d (unsigned): ", num);
+			printBinary((unsigned int)num, bits);
+		}
+	}
+	else {
+		printf("The number %d cannot be put into %d bytes in %s format.\n",
+			num, bytes, (signedFlag ? "signed" : "unsigned"));
+	}
 	return 0;
 }
